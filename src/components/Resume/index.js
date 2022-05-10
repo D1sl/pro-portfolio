ae0279b3e622c0225a0b94954677db9f875fd9ec
D1sl <benjaminmolini@gmail.com>
2022-05-09 20:57:47 -0500

    adds summary into the resume -section

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -3,7 +3,33 @@ import React from 'react'
 function Resume() {
     return (
         <div className="container">
-            <h1>My Resume</h1>
+            <h1>Summary</h1>
+            <p>Creative individual with a strong, demonstrated history of over 10 years in graphic design as well as diverse experience in team leading and project management.</p>
+            <p>What motivates me the most is being able to see the concrete impacts of my work while doing something I am passionate about and creating something that aligns with my clients goals and expectations to make a positive impact and successfully drive their business forward.</p>
+            <p>Through extensive experience in various customer-oriented positions, I have accumulated valuable knowledge that can be seen in the results of projects I’ve worked on. I have great instincts, but being an analytical person, I always seek data for my decisions. Paying a lot of attention in research, I am good at executing even big ideas within given constraints.</p>
+            <h1>Highlights</h1>
+            <div className="resume-highlights">
+                <div>
+                    <ul>
+                        <li>Strategic Planning Experience</li>
+                        <li>Project Management</li>
+                        <li>Team Leadership</li>
+                        <li>Data Analysis</li>
+                        <li>Retail Service Plan Design and Execution</li>
+                    </ul>
+                </div>
+                <div>
+                    <ul>
+                        <li>Technologically Savvy</li>
+                        <li>Comprehensive Organization Skills</li>
+                        <li>Strong Team Player</li>
+                        <li>Excellent Problem Solver</li>
+                        <li>Results Oriented Leader</li>
+                        <li>Customized Solutions Capabilities</li>
+                    </ul>
+                </div>
+            </div>
+            <h1>Work Experience</h1>
         </div>
     )
 }
