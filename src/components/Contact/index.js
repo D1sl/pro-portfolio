ad6b0e3901fdf38391e6f50fcee7e9c8444b4613
D1sl <94911312+D1sl@users.noreply.github.com>
2022-05-10 18:43:30 -0500

    Adds a contact form on the page

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,11 +2,23 @@ import { useState } from "react";
 
 function Contact() {
     const [contactFormState, setContactFormState] = useState({
-        subject: "",
         name: "",
+        subject: "",
         message: ""
     });
     const [errorMessage, setErrorMessage] = useState("");
+    const { name, subject, message } = contactFormState;
+
+    function handleChage(e) {
+        if (!e.target.value.length) {
+            setErrorMessage(`${e.target.name} is required.`);
+        } else {
+            setErrorMessage("");
+        }
+        if (!errorMessage) {
+            setContactFormState({ ...contactFormState, [e.target.name]: e.target.value });
+        }
+    }
 
     return (
         <div className="container">
@@ -14,11 +26,24 @@ function Contact() {
             <h2>contact<br /><span>me</span></h2>
             <p className="desc">To get into contact with me, fill out the form below!</p>
         </div>
-        <a className='downloadbutton' href='../../assets/downloads/resume.pdf' download>
-            <div>
-                hello
+            <div className="contactform">
+                <form id="contact">
+                    <div className="form">
+                        <div>
+                            <label htmlFor="name">Name</label>
+                            <input type="text" name="name" defaultValue={name} onBlur={handleChage} />
+                        </div>
+                        <div>
+                            <label htmlFor="subject">subject</label>
+                            <input type="subject" name="subject" defaultValue={subject} onBlur={handleChage} />
+                        </div>
+                        <div>
+                            <label htmlFor="message">Message</label>
+                            <textarea name="message" rows="7" defaultValue={message} onBlur={handleChage} />
+                        </div>
+                    </div>
+                </form>
             </div>
-        </a>
     </div>        
     )
 }
