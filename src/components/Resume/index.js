108000f13ccd76e0eec84f108ce6435819bc70c2
D1sl <94911312+D1sl@users.noreply.github.com>
2022-05-11 10:36:18 -0500

    spell fixes

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -5,7 +5,7 @@ function Resume() {
         <div className="container">
             <div className="row">
                 <h2>my<br /><span>resume</span></h2>
-                <p className='resdesc'><strong>Front-End</strong> | HTML, CSS, JavaScript, jQuery, Bootstra, React, Responsive Design</p>
+                <p className='resdesc'><strong>Front-End</strong> | HTML, CSS, JavaScript, jQuery, Bootstrap, React, Responsive Design</p>
                 <p className='resdesc'><strong>Back-End</strong> | Node.js, Express.js, REST, SQL, MongoDB</p>
             </div>
             <a className='downloadbutton' href='../../assets/downloads/resume.pdf' download>
