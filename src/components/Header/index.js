51d551cf0a00898f1cbbef0ebfee0d3e6d206ef7
d1sl <benjaminmolini@gmail.com>
2022-09-12 21:44:32 +0100

    Removed unused imports

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,33 +1,30 @@
 import { NavLink } from 'react-router-dom';
 
-function Header(props) {
-    const { currentPage, setCurrentPage } = props;
-
+function Header() {
     return (
-
         <nav>
-        <div className="container">
-            <div className="brand">
-                <h1>molini</h1>
+            <div className="container">
+                <div className="brand">
+                    <h1>molini</h1>
+                </div>
+                <div className="menu">
+                    <ul>
+                        <li className='menu-item'>
+                            <NavLink to="/">about me</NavLink>
+                        </li>
+                        <li className='menu-item'>
+                            <NavLink to="/work">my work</NavLink>
+                        </li>
+                        <li className='menu-item'>
+                            <NavLink to="/resume">my resume</NavLink>
+                        </li>
+                        <li className='menu-item'>
+                            <NavLink to="/contact">contact me</NavLink>
+                        </li>
+                    </ul>
+                </div>
             </div>
-            <div className="menu">
-                <ul>
-                    <li className='menu-item'>
-                        <NavLink to="/">about me</NavLink>
-                    </li>
-                    <li className='menu-item'>
-                        <NavLink to="/work">my work</NavLink>
-                    </li>
-                    <li className='menu-item'>
-                        <NavLink to="/resume">my resume</NavLink>
-                    </li>
-                    <li className='menu-item'>
-                        <NavLink to="/contact">contact me</NavLink>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+        </nav>
     )
 }
 
